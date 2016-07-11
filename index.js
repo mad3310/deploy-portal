574a96b378bd11677c0e31252e2085b697116528
dongwanlong <dongwanlong@le.com>
2016-07-11 11:48:42 +0800

    代理修改

--- index.js
+++ index.js
@@ -2,15 +2,3 @@
  * Created by dongwanlong on 2016/7/8.
  */
 var app = require('./src/app.js');
-//var fs = require('fs');
-//var path = require('path');
-//
-//var pidfile = path.join(__dirname, 'run/app.pid');
-//fs.writeFileSync(pidfile, process.pid);
-//
-//process.on('SIGTERM',function(){
-//    if(fs.existsSync(pidfile)){
-//        fs.unlinkSync(pidfile);
-//    }
-//    process.exit(0);
-//});
